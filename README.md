d62778e5c2372e2f44308f4b2c5f23ddaea66009
Your Name <you@example.com>
2022-12-04 14:53:54 +0100

    Added wasm

--- README.md
+++
@@ -1,1 +0,0 @@
-# Alexlexlexlex.github.io
--- script/script.js
+++
@@ -1,4 +0,0 @@
-function getText() {
-    let name = document.getElementById("text").value;
-    document.getElementById("res").innerHTML = name + "'s site";
-}
